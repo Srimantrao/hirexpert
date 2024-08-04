86b64e9bbc49b8213db456ce9cce333f9a46ca9f
Srimantrao <srimantraosonar@gmail.com>
2024-08-04 17:17:18 +0530

    03-Aug-2024

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,6 @@ import 'package:hirexpert/controller/User_Controller/Candidate_Controller/Datail
 import 'package:hirexpert/controller/User_Controller/Candidate_Controller/OTP_Controller/OTP_Controller.dart';
 import 'package:hirexpert/controller/User_Controller/Candidate_Controller/Profile_Info_Controller/Document_Info_Controller.dart';
 import 'package:hirexpert/controller/User_Controller/Candidate_Controller/SettingController/Setting_Screen_Controller.dart';
-import 'package:hirexpert/view/screen/Candidate/collection/location.dart';
 import 'package:hirexpert/view/screen/splash/logo.dart';
 import 'package:hirexpert/view/utils/app_color.dart';
 import 'package:hirexpert/view/utils/app_constance.dart';
@@ -140,7 +139,7 @@ class MyApp extends StatelessWidget {
           ),
         ),
         debugShowCheckedModeBanner: false,
-        home: Location(),
+        home: Logo(),
       ),
     );
   }
